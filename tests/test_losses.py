eaba2da1e11ac9573a9f944ecc0d1932249acf12
Pierre-Louis Nordmann <pierrelouis.nordmann@gmail.com>
2023-11-19 13:13:50 +0000

    fix: updated tests

--- tests/test_losses.py
+++ tests/test_losses.py
@@ -1,14 +1,7 @@
 import jax
 import jax.numpy as jnp
-import pytest
 
-from sketch_wavenet.gaussian_mixture import inner_mixture_loss
-
-
-@pytest.fixture(scope="session")
-def jax_cpu():
-    with jax.default_device(jax.devices("cpu")[0]):
-        yield
+from sketch_wavenet.losses import build_mask, inner_mixture_loss
 
 
 def test_inner_mixture_loss_one_gaussian():
@@ -42,3 +35,26 @@ def test_inner_mixture_loss_one_gaussian():
         )
     )
     assert jnp.allclose(got, want, atol=eps).item()
+
+
+def test_build_mask():
+    N_max = 6
+    N_s = jnp.array([3, 4, 1, 1, 6])
+    batch_size = N_s.shape[0]
+
+    mask = build_mask(N_max, N_s)
+
+    h, w = mask.shape
+    assert h == batch_size
+    assert w == N_max
+
+    comparison = mask == jnp.array(
+        [
+            [1, 1, 1, 0, 0, 0],
+            [1, 1, 1, 1, 0, 0],
+            [1, 0, 0, 0, 0, 0],
+            [1, 0, 0, 0, 0, 0],
+            [1, 1, 1, 1, 1, 1],
+        ]
+    )
+    assert jnp.sum(comparison).item() == batch_size * N_max
